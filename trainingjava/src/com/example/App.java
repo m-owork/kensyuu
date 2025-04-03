86825bd3955010f196e72d33a00c4303a40e72df
admin <admin@192.168.40.162>
2025-04-03 19:10:03 +0900

    164ptest1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-    	int[] scores = {20, 30, 40, 50, 80};
-    	int sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
-    	int avg = sum / scores.length;
-    	System.out.println("合計点" + sum);
-    	System.out.println("平均点" + avg);
-    }
+int[] arrayA = {1, 2, 3};
+int[] arrayB;
+arrayB = arrayA;
+arrayB[0] = 100;
+System.out.println(arrayA[0]);
+}
 }
