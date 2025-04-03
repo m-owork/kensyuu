03137ea4e46f1cf66960712ca8c56ad51137253a
admin <admin@192.168.40.162>
2025-04-03 18:24:14 +0900

    3章総合問題test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,20 +2,17 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-    	System.out.println("運勢を占おう");
-int fortune = new java.util.Random().nextInt(8);
-switch (fortune) {
-case 8:
-	System.out.println("大変良い");
-	break;
-case 7:
-	System.out.println("良い");
-	break;
-case 6:
-	System.out.println("ほどほど良い");
-	break;
-	default:
-		System.out.println("普通");
+    	int isHungry = 1;
+    	String food = "ホットドッグ";
+    	System.out.println("こんにちは");
+    	if (isHungry == 0) {
+    	System.out.println("お腹がいっぱいです");
+    		} else {
+    			System.out.println("はらぺこです");
+    		}
+    	if (isHungry == 1) {
+    	System.out.println(food + "をいただきます");
     }
-    }
+    	System.out.println("ごちそうさまでした");
+    	}
 }
