de4ab2a42654eabc5b13aaffec42fcf959c0b8f7
admin <admin@192.168.40.162>
2025-04-03 15:12:16 +0900

    switch_break_test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,9 +2,20 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-   int age = 18;
-   int month = 6;
-if (age >= 18 && month == 5);
-	System.out.println("当たりです");
+    	System.out.println("運勢を占おう");
+int fortune = new java.util.Random().nextInt(8);
+switch (fortune) {
+case 8:
+	System.out.println("大変良い");
+	break;
+case 7:
+	System.out.println("良い");
+	break;
+case 6:
+	System.out.println("ほどほど良い");
+	break;
+	default:
+		System.out.println("普通");
+    }
     }
 }
