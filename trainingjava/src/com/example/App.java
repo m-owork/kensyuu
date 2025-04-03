166f52a0491a9ed1d8fb907228330c70a3000e7a
admin <admin@192.168.40.162>
2025-04-03 18:31:20 +0900

    3章総合問題test2

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,17 +2,22 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-    	int isHungry = 1;
-    	String food = "ホットドッグ";
-    	System.out.println("こんにちは");
-    	if (isHungry == 0) {
-    	System.out.println("お腹がいっぱいです");
-    		} else {
-    			System.out.println("はらぺこです");
-    		}
-    	if (isHungry == 1) {
-    	System.out.println(food + "をいただきます");
+    	System.out.println("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+    	int selected = new java.util.Scanner(System.in).nextInt();
+    	switch (selected) {
+    	case 1:
+    		System.out.println("検索します");
+    		break;
+    	case 2:
+    		System.out.println("登録します");
+    		break;
+    	case 3:
+    		System.out.println("削除します");
+    		break;
+    	case 4:
+    		System.out.println("変更します");
+    		break;
     }
-    	System.out.println("ごちそうさまでした");
-    	}
 }
+}
+    		
