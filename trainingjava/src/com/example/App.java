145d2e608064bad3d4be0ee90ad04ab117e0010e
admin <admin@192.168.40.162>
2025-04-03 15:03:37 +0900

    条件式test2

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,14 +2,9 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-    	boolean doorClose = false;
-    	if (doorClose != true) {
-    		System.out.println("ドアを開ける");
-    	while (doorClose == true) {
-    		System.out.println("ノックする");
-    		System.out.println("1分待つ");
-
-    	}
-    }
+   int age = 18;
+   int month = 6;
+if (age >= 18 && month == 5);
+	System.out.println("当たりです");
     }
 }
