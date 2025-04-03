9c384f243dcc71640410f701a610613d046a48d0
admin <admin@192.168.40.162>
2025-04-03 13:57:12 +0900

    条件式(104pに自分で手を加えたもの修正版)_false

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,13 +2,14 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-    	boolean doorClose = true;
+    	boolean doorClose = false;
+    	if (doorClose != true) {
+    		System.out.println("ドアを開ける");
     	while (doorClose == true) {
     		System.out.println("ノックする");
     		System.out.println("1分待つ");
-    		if (doorClose != true) {
-    			System.out.println("ドアを開ける");
-    		}
+
     	}
     }
-    	}
+    }
+}
