7e2002f78eb34887a0bbcb03197b8ad15a146413
admin <admin@192.168.40.162>
2025-04-04 19:12:14 +0900

    5章総合問題5-4test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,19 +2,21 @@ package com.example;
 
 public class App {
 public static void main(String[] args) {
-	String address = "nantoka@kantoka.com";
-	String text = "お問い合わせいただき誠にありがとうございます。";
-	email(address, text);
-	}
-public static void email(String address, String text) {
-	System.out.println(address + "に、以下のメールを送信しました");
-	System.out.println("件名:無題");
-	System.out.println("本文:" + text);
+		double TriangleArea = calcTriangleArea(20.0, 5.0);
+		System.out.println("三角形の底辺の長さが20.0cm、高さが5.0cmの場合、面積は"
+		+ TriangleArea + "平方cmである。");
+		double CircleArea = clacCircleArea(12.0);
+		System.out.println("円の半径が12.0cmの場合、面積は"
+		+ CircleArea + "平方cmである。");
 }
-	public static void email(String title, String address, String text) {
-		System.out.println(address + "に以下のメールを送信しました");
-		System.out.println("件名:" + title);
-		System.out.println("本文:" + text);
 		
-	}
+	public static double calcTriangleArea(double bottom, double height) {
+	double area = (bottom * height) / 2;
+	return area;
+}
+
+public static double clacCircleArea(double radius) {
+	double area = radius * radius * 3.14;
+	return area;
+}
 }
