b9d50379ae71a65d3bc083e5fe56f021c76809c5
admin <admin@192.168.40.162>
2025-04-04 18:52:39 +0900

    5章総合問題5-3test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,13 +2,17 @@ package com.example;
 
 public class App {
 public static void main(String[] args) {
-	String title = "お問い合わせの件について";
 	String address = "nantoka@kantoka.com";
 	String text = "お問い合わせいただき誠にありがとうございます。";
-	email(title, address, text);
+	email(address, text);
 	}
+public static void email(String address, String text) {
+	System.out.println(address + "に、以下のメールを送信しました");
+	System.out.println("件名:無題");
+	System.out.println("本文:" + text);
+}
 	public static void email(String title, String address, String text) {
-		System.out.println("メールの宛先アドレスに以下のメールを送信しました");
+		System.out.println(address + "に以下のメールを送信しました");
 		System.out.println("件名:" + title);
 		System.out.println("本文:" + text);
 		
