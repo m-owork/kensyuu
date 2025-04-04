f72b728b6a0bf24a9fff076145fd89544135f13f
admin <admin@192.168.40.162>
2025-04-04 18:02:44 +0900

    引数と配列test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -1,19 +1,13 @@
 package com.example;
 
 public class App {
-public static int add (int x, int y) {
-	return x + y;
-}
-public static double add(double x, double y) {
-	return x + y;
-}
-public static String add(String x, String y) {
-	return x + y;
+public static void printArray(int[] array) {
+	for (int element : array) {
+		System.out.println(element);
+	}
 }
 public static void main(String[] args) {
-	System.out.println(add(10, 30));
-	System.out.println(add(5.8, 4.2));
-	System.out.println(add("こんにちは", "いいお天気ですね"));
-	
-}
+	int[] array = {1, 2, 3};
+	printArray(array);
+	}
 }
