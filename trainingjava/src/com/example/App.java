117830103a0e825aff4b9a563a3839f3277aee8a
admin <admin@192.168.40.162>
2025-04-04 13:30:34 +0900

    4章復習test2

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,9 +2,9 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-    	int[] scores;
-    	scores = new int[5];
-    	scores[1] = 30;
-    	System.out.println(scores[1]);
+int [] scores = {20, 30, 40, 50, 80};
+for (int i = 0; i < scores.length; i++) {
+	System.out.println(scores[i]);
+}
     }
 }
