4adfa5ad58d4ba4878a1197138956c4127ce29ea
admin <admin@192.168.40.162>
2025-04-04 18:15:16 +0900

    配列と戻り値test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -1,17 +1,17 @@
 package com.example;
 
 public class App {
-public static void incArray(int[] array) {
-	for(int i = 0; i < array.length; i++) {
-		array[i]++;
+public static int[] makeArray(int size) {
+	int[] newArray = new int[size];
+	for(int i = 0; i < newArray.length; i++) {
+		newArray[i] = i;
 	}
+	return newArray;
 }
 public static void main(String[] args) {
-	int[] array = {1, 2, 3};
-	incArray(array);
+	int[] array = makeArray(3);
 	for(int i : array) {
-		System.out.
-		println(i);
+		System.out.println(i);
 	}
 	}
 }
