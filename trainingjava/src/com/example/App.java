7977dc5a26b8e8b86ea7c33ba6e07c27c0a5c620
admin <admin@192.168.40.162>
2025-04-04 17:07:17 +0900

    overload_test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -1,12 +1,19 @@
 package com.example;
 
 public class App {
-	public static int add(int x, int y) {
-    	int answer = x + y;
-    	return answer;
-    }
-    public static void main(String[] args) {
-    	int answer = add(200, 10);
-    	System.out.println("100 + 10 = " + answer);
-    }
-    }
+public static int add (int x, int y) {
+	return x + y;
+}
+public static double add(double x, double y) {
+	return x + y;
+}
+public static String add(String x, String y) {
+	return x + y;
+}
+public static void main(String[] args) {
+	System.out.println(add(10, 30));
+	System.out.println(add(5.8, 4.2));
+	System.out.println(add("こんにちは", "いいお天気ですね"));
+	
+}
+}
