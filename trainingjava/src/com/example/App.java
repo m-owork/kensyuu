20fbf9f5db3776587af2362ef50a8a54017b4282
admin <admin@DESKTOP-R986KR1>
2025-04-07 15:23:21 +0900

    10章386p_test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -3,8 +3,9 @@ package com.example;
 public class App {
 public static void main(String[] args) {
 	exam1 sh = new exam1();
-	sh.run();
 	exam2 h = new exam2();
-	h.run();
+	Matango m = new Matango();
+	sh.fly();
+	sh.attack(m);
 }
 }
--- trainingjava/src/com/example/exam1.java
+++ trainingjava/src/com/example/exam1.java
@@ -5,13 +5,23 @@ public class exam1 extends exam2 {
 	boolean flying;
 	public void fly() {
 		this.flying = true;
-		System.out.println("飛び上がった");
+		System.out.println(this.name + "は飛び上がった");
 	}
 	public void land() {
 		this.flying = false;
-		System.out.println("着地した");
+		System.out.println(this.name + "は着地した");
 	}
 	public void run() {
 		System.out.println(this.name + "は撤退した");
+	}
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた");
+		if (this.flying) {
+			System.out.println(this.name + "の飛翔攻撃");
+			m.hp -= 5;
+			System.out.println("更に5ポイントのダメージを与えた");
+	}
 	}
 }
