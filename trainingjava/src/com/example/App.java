d62f5615292fd95959d9afbe9c23d5bdf10d7fc0
admin <admin@DESKTOP-R986KR1>
2025-04-07 10:20:37 +0900

    クラス作成test1

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,21 +2,9 @@ package com.example;
 
 public class App {
 public static void main(String[] args) {
-		double TriangleArea = calcTriangleArea(20.0, 5.0);
-		System.out.println("三角形の底辺の長さが20.0cm、高さが5.0cmの場合、面積は"
-		+ TriangleArea + "平方cmである。");
-		double CircleArea = clacCircleArea(12.0);
-		System.out.println("円の半径が12.0cmの場合、面積は"
-		+ CircleArea + "平方cmである。");
-}
-		
-	public static double calcTriangleArea(double bottom, double height) {
-	double area = (bottom * height) / 2;
-	return area;
-}
-
-public static double clacCircleArea(double radius) {
-	double area = radius * radius * 3.14;
-	return area;
+int a = 10; int b= 2;
+int total = CalcLogic.tasu(a, b);
+int delta = CalcLogic.hiku(a, b);
+System.out.println("足すと" + total + "、引くと + delta");
 }
 }
