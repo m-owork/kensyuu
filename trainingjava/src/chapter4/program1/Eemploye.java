2d14fa39ab4c4d85406da62843cf2d20ea795594
admin <admin@192.168.40.162>
2025-04-10 18:55:33 +0900

    実装力チェックテスト_04_クラスの継承test2

--- trainingjava/src/chapter4/program1/Eemploye.java
+++ trainingjava/src/chapter4/program1/Eemploye.java
@@ -3,7 +3,7 @@ package chapter4.program1;
 public class Eemploye {
 	private String name = "A";
 	private String department = "エンジニア";
-	private String info = "氏名: + name +  + 所属: + department";
+	private String info = "氏名:" + name + " "+ "所属:" + department;
 	
 	public String getName() {
 		return this.name; 
